fbaefd4afd0c8c5d0abbce0b24a7ff9af358f281
smalltasi <11222two@gmail.com>
2021-07-14 15:12:53 +0800

    fianl2

--- write.py
+++ write.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
+	price = int(price)
 	p = [name, price]
 	product.append(p)
 #  也可以寫成  product.append([name, price])
@@ -15,4 +16,4 @@ for p in product:
 
 with open('product.txt', 'w') as f:
 	for pd in product:
-		f.write(pd[0] + ' 的價格是 ' + pd[1] + '\n')
+		f.write(pd[0] + ' 的價格是 ' + str(pd[1]) + '\n') #pd[1]原是整數再轉為字串
