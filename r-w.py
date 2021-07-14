96e50df9fb427a50073b5f39eec68e15004bb856
smalltasi <11222two@gmail.com>
2021-07-14 20:33:12 +0800

    realfinal

---
+++ r-w.py
@@ -0,0 +1,35 @@
+import os
+
+
+product = []
+
+if os.path.isfile('product.csv'):
+
+	with open('product.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			price = int(price)
+			product.append([name, price])
+else:
+	print()
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	price = int(price)
+	p = [name, price]
+	product.append(p)
+#  也可以寫成  product.append([name, price])
+print(product)
+
+for p in product:
+	print(p)
+	print(p[0], '的價格是', p[1])
+
+with open('product.csv', 'w', encoding='utf-8') as f:   #csv要加編碼 通用utf-8
+	f.write('商品,價格\n')
+	for pd in product:
+		f.write(pd[0] + ',' + str(pd[1]) + '\n') #pd[1]原是整數再轉為字串
